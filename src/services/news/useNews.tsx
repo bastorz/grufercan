2e586d70d51909e3326e871f7d9b7fa1abcca4e1
Vincent <vincent@medanosol.es>
2021-12-14 11:34:46 +0000

    prod

--- src/services/news/useNews.tsx
+++ src/services/news/useNews.tsx
@@ -1,14 +1,14 @@
-import { useEffect } from 'react';
-import { useState } from 'react';
+import { useEffect } from "react";
+import { useState } from "react";
 
 export const useNews = () => {
   const [news, setNews] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const getData = () => {
-    fetch('/grufercan/api/db/news/noticia1.json', {
-      // fetch('/api/db/news/noticia1.json', {
+    // fetch('/grufercan/api/db/news/noticia1.json', {
+    fetch("/api/db/news/noticia1.json", {
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
     })
       .then(async (response) => {
