32d393620c8408761975737956e87b2335b5c64b
David <david@medanosol.es>
2022-01-05 13:29:36 +0000

    refactor and translate cookie dialog

--- src/_app.tsx
+++ src/_app.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import '../i18n';
+import { CookiesDialog } from './components/CookiesDialog';
 import './styles/index.css';
 const App = ({ children }: any) => {
   console.log('Running App!');
   return (
-    <React.Suspense fallback={<div>Loading!</div>}>{children}</React.Suspense>
+    <React.Suspense fallback={<div>Loading!</div>}>
+      <div className="relative">
+        <CookiesDialog /> {children}
+      </div>
+    </React.Suspense>
   );
 };
 export default App;
