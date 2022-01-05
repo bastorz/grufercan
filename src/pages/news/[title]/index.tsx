98f38cf9da99dac1700a41caf5fc563f6f8a32ea
David <david@medanosol.es>
2022-01-05 14:52:44 +0000

    news see more section

--- src/pages/news/[title]/index.tsx
+++ src/pages/news/[title]/index.tsx
@@ -9,7 +9,6 @@ import { useNews } from '../../../services/news/useNews';
 
 const News: React.FC = (newsProps: any) => {
   const currentNewsId = newsProps?.location.state.newsId;
-
   const formatDate = (date: string): string => {
     const timestamp = Date.parse(date);
     return new Date(timestamp).toLocaleDateString();
@@ -17,14 +16,16 @@ const News: React.FC = (newsProps: any) => {
   const { news, isLoading } = useNews();
   const [filteredNews, setFilteredNews] = useState<any[]>();
   const [currentNews, setCurrentNews] = useState<any>();
+  const [visibleNews, setVisibleNews] = useState(2);
   const { language } = useLanguage();
-
+  const { t } = useTranslation();
   useEffect(() => {
     if (!isLoading && news) {
       setFilteredNews(news.filter((n) => n.id !== currentNewsId));
       setCurrentNews(news.find((n) => n.id === currentNewsId));
     }
   }, [news, isLoading, newsProps]);
+
   return (
     <>
       <SEO
@@ -35,7 +36,7 @@ const News: React.FC = (newsProps: any) => {
       <div className="flex flex-col">
         <div className="flex flex-col w-full py-20">
           <div className="w-full px-10 py-10 text-6xl lg:px-32 lg:text-9xl text-primary">
-            <h1>NOTICIAS</h1>
+            <h1>{t('news.news')}</h1>
           </div>
           {currentNews && (
             <div className="grid grid-cols-5 gap-10 md:gap-0">
@@ -54,7 +55,7 @@ const News: React.FC = (newsProps: any) => {
                   className="w-full aspect-square"
                 />
                 <div className="absolute top-0 block lg:hidden md:right-5 right-20">
-                  <div className="w-full p-8 text-left  bg-primary">
+                  <div className="w-full p-8 text-left bg-primary">
                     <span className="text-white md:text-3xl lg:text-4xl">
                       <span>{formatDate(currentNews.date)}</span>
                     </span>
@@ -62,7 +63,7 @@ const News: React.FC = (newsProps: any) => {
                 </div>
               </div>
               <div className="hidden lg:block lg:col-span-1">
-                <div className="w-full p-8 text-left  bg-primary aspect-square">
+                <div className="w-full p-8 text-left bg-primary aspect-square">
                   <span className="text-white md:text-3xl lg:text-4xl">
                     <span>{formatDate(currentNews.date)}</span>
                   </span>
@@ -74,15 +75,15 @@ const News: React.FC = (newsProps: any) => {
 
         {filteredNews && (
           <div className="grid grid-cols-1 gap-10 px-10 md:grid-cols-2 md:px-32 text-primary">
-            {filteredNews.map((news, i) => (
+            {filteredNews.slice(0, visibleNews).map((news, i) => (
               <div className="relative flex flex-col gap-4" key={i}>
                 <img
                   src={`http://localhost:8001/uploads/${news.imgUrl}`}
                   alt={news.title}
                   className="w-full aspect-video"
                 />
-                <div className="absolute right-0  top-5">
-                  <div className="w-full px-6 py-2 text-center  bg-secondary text-primary">
+                <div className="absolute right-0 top-5">
+                  <div className="w-full px-6 py-2 text-center bg-secondary text-primary">
                     <span className="text-xl font-bold lg:text-2xl">
                       {formatDate(news.date)}
                     </span>
@@ -99,12 +100,23 @@ const News: React.FC = (newsProps: any) => {
                   state={{ newsId: news.id }}
                 >
                   <div className="flex items-center justify-end w-full gap-10 cursor-pointer">
-                    <span className="text-xl font-bold">SEGUIR LEYENDO</span>
+                    <span className="text-xl font-bold">
+                      {t('home.sections.latestNews.keepReading')}
+                    </span>
                     <Icon path={mdiChevronDownCircleOutline} size={2} />
                   </div>
                 </Link>
               </div>
             ))}
+          </div>
+        )}
+        {filteredNews && filteredNews.length !== visibleNews && (
+          <div
+            className="flex flex-col items-center justify-center w-full pt-20 cursor-pointer text-primary"
+            onClick={() => setVisibleNews(visibleNews + 2)}
+          >
+            <span className="text-2xl">{t('news.showMore')}</span>
+            <Icon path={mdiChevronDownCircleOutline} size={4} />
           </div>
         )}
       </div>
